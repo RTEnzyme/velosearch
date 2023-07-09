6affcc8ae949ef4e1d178a97b17cf1ea8d79a598
RTEnzyme <408215131@qq.com>
2023-07-09 09:36:50 +0800

    feat: impl code gen

--- src/optimizer/physical_optimizer/partition_predicate_reorder.rs
+++ src/optimizer/physical_optimizer/partition_predicate_reorder.rs
@@ -37,6 +37,7 @@ impl PhysicalOptimizerRule for PartitionPredicateReorder {
                         .enumerate()
                         .map(|(i, f)| (f.name().as_str(), i))
                         .collect();
+                    println!("{:?}", term2idx);
                     if let Some(ref cnf) = predicate.cnf_predicates {
                         let reorder_predicate: HashMap<usize, Arc<dyn PhysicalExpr>> = stats
                         .iter()
